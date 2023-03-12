99c452392e8f1917ff5f5246c0792872442ddeae
VivianOsorio <v31.osorio@gmail.com>
2023-03-11 19:09:05 -0800

    fixed video scale, about section image

--- README.md
+++
@@ -1,7 +0,0 @@
-# LandingPage
-Landing page project with sass
-
-npm install 
-npm install node-sass 
-
-npm run sass
---
+++ js/animations.js
@@ -0,0 +1,7 @@
+gsap.from(".material-symbols-outlined",{
+    y: "-30px", 
+    repeat: -1, 
+    yoyo: true, 
+    duration: 1.5,  
+    ease: "power1.inOut"
+});
